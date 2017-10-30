9ee0975d6eedc74ba4580053d37135e10af19e78
Jacob Lanham <jacob.lanham@my.tccd.edu>
2017-10-30 12:46:00 -0500

    Added Lab 3

---
+++ Labs/Lab3/MovieLib.Data.Memory/MovieDatabase.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Jacob Lanham
+ * ITSE 1430
+ * 10-29-2017
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLib.Data.Memory
+{
+    public abstract class MovieDatabase : IMovieDatabase
+    {
+        /// <summary>Adds a movie.</summary>
+        /// <param name="movie">The movie to add.</param>
+        /// <returns>The added movie.</returns>
+        public Movie Add( Movie movie )
+        {
+            if (movie == null)
+                return null;
+
+            //Validate using IValidatable Object
+            if (!ObjectValidator.TryValidate(movie, out var errors))
+                return null;
+
+            return AddCore(movie);
+        }
+
+        /// <summary>Get a specfic moive.</summary>
+        /// <param name="id">The movies Id.</param>
+        /// <returns>The movie, if it exsists.</returns>
+        public Movie Get( int id )
+        {
+            if (id <= 0)
+                return null;
+
+            return GetCore(id);
+        }
+
+        /// <summary>Gets all the movies.</summary>
+        /// <returns>The movies.</returns>
+        public IEnumerable<Movie> GetAll()
+        {
+            return GetAllCore();
+        }
+
+        /// <summary>Removes the movie.</summary>
+        /// <param name="id">The movie to remove.</param>
+        public void Remove( int id )
+        {
+            if (id <= 0)
+                return;
+
+            RemoveCore(id);
+        }
+
+        /// <summary>Updates the movie.</summary>
+        /// <param name="movie">The movie to update</param>
+        /// <returns>The updated movie</returns>
+        public Movie Update( Movie movie )
+        {
+            if (movie == null)
+                return null;
+
+            //Validate using IValidatable Object
+            if (!ObjectValidator.TryValidate(movie, out var errors))
+                return null;
+
+            var existing = GetCore(movie.Id);
+            if (existing == null)
+                return null;
+
+            return UpdateCore(existing, movie);
+
+
+
+        }
+
+        protected abstract Movie GetCore( int id );
+        protected abstract IEnumerable<Movie> GetAllCore();
+        protected abstract void RemoveCore( int id );
+        protected abstract Movie UpdateCore( Movie existing, Movie movie );
+        protected abstract Movie AddCore( Movie movie );
+    }
+}
---
+++ Labs/Lab3/MovieLib.Windows/MainForm.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * Jacob Lanham
+ * ITSE 1430
+ * 10-29-2017
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieLib.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            _gridMovies.AutoGenerateColumns = false;
+
+            UpdateList();
+        }
+
+        private Movie GetSelectedMovie ()
+        {
+            if (_gridMovies.SelectedRows.Count > 0)
+                return _gridMovies.SelectedRows[0].DataBoundItem as Movie;
+
+            return null;
+        }
+
+        private void UpdateList ()
+        {
+            _bsMovies.DataSource = _database.GetAll().ToList();
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void OnMoviesAdd( object sender, EventArgs e )
+        {
+            var child = new MovieDetailForm();
+            if (child.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            //Save Movie
+            _database.Add(child.Movie);
+            UpdateList();
+        }
+
+        private void OnMovieEdit( object sender, EventArgs e )
+        {
+            var movie = GetSelectedMovie();
+
+            //If there is no movie, show error dialog.
+            if (movie == null)
+            {
+                MessageBox.Show(this, "No movie to edit", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            EditMovie(movie);
+        }
+
+        private void EditMovie(Movie movie)
+        {
+            var child = new MovieDetailForm();
+            child.Movie = movie;
+
+            if (child.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            //Save Movie
+            _database.Update(child.Movie);
+            UpdateList();
+        }
+
+        private void OnMoviesDelete( object sender, EventArgs e )
+        {
+            var movie = GetSelectedMovie();
+            //If there is no movie, show error dialog.
+            if (movie == null)
+            {
+                MessageBox.Show(this, "No movie to delete", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DeleteMovie(movie);
+        }
+
+        private void DeleteMovie(Movie movie)
+        {
+            //Confirm Delete
+            if ((MessageBox.Show(this, $"Are you sure you want to delete {movie.Title}?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No))
+                return;
+
+            //Deletes Movie
+            _database.Remove(movie.Id);
+            UpdateList();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            var box = new AboutBox();
+            box.ShowDialog();
+        }
+
+        private void OnKeyDownGrid( object sender, KeyEventArgs e )
+        {
+            var movie = GetSelectedMovie();
+            if (e.KeyCode == Keys.Delete && movie != null)
+                DeleteMovie(movie);
+            else if (e.KeyCode == Keys.Enter && movie != null)
+                EditMovie(movie);
+
+        }
+
+        private void OnEditRow( object sender, DataGridViewCellEventArgs e )
+        {
+            var grid = sender as DataGridView;
+
+            //Handle column clicks
+            if (e.RowIndex < 0)
+                return;
+
+            var row = grid.Rows[e.RowIndex];
+            var item = row.DataBoundItem as Movie;
+
+            if (item != null)
+                EditMovie(item);
+        }
+
+        private IMovieDatabase _database = new MovieLib.Data.Memory.MemoryMovieDatabase();
+    }
+}
