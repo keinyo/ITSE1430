814def44399e7973ad4029ea1c926706fd925a45
Jacob Lanham <jacob.lanham@my.tccd.edu>
2017-11-15 18:31:19 -0600

    Updated ClassWork

--- ClassWork/Section4/Nile.Stores.Sql/SqlProductDatabase.cs
+++ ClassWork/Section4/Nile.Stores.Sql/SqlProductDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//TODO: check for error in this program, suspsected file: ProductDatabase.cs
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -21,7 +22,20 @@ namespace Nile.Stores.Sql
         }
         protected override Product AddCore( Product product )
         {
-            throw new NotImplementedException();
+            var id = 0;
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("AddProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.AddWithValue("@description", product.Description); //Prefered approach
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
+
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            };
+
+            return GetCore(id);
         }
 
         protected override IEnumerable<Product> GetAllCore()
@@ -43,11 +57,11 @@ namespace Nile.Stores.Sql
                         var product = new Product() {
                             Id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
                             Name = reader.GetFieldValue<string>(1),
-                            Description = reader.GetString(3),
                             Price = reader.GetDecimal(2),
+                            Description = reader.GetString(3),                           
                             IsDiscontinued = reader.GetBoolean(4),
                         };
-                    }
+                    };
                 };
 
                     return products;
@@ -56,18 +70,71 @@ namespace Nile.Stores.Sql
 
         protected override Product GetCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("GetProduct", conn) { CommandType = CommandType.StoredProcedure };
+                cmd.Parameters.AddWithValue("@id", id);
+
+                var ds = new DataSet();
+                var da = new SqlDataAdapter() {
+                    SelectCommand = cmd
+                };
+
+                da.Fill(ds);
+
+                var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
+                if (table != null)
+                {
+                    var row = table.AsEnumerable().FirstOrDefault();
+                    if (row != null)
+                    {
+                        return new Product() {
+                            Id = Convert.ToInt32(row["id"]),
+                            Name = row.Field<string>("Name"), // Prefered
+                            Description = row.Field<string>("Description"),
+                            Price = row.Field<decimal>("price"),
+                            IsDiscontinued = row.Field<bool>("isdiscontinued"),
+                        };
+                    };
+                };
+
+            };
+            return null;
         }
 
         protected override void RemoveCore( int id )
         {
-            throw new NotImplementedException();
+            using (var conn = OpenDatabase())
+            {
+                //Alternative approach to creating connection.
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "RemoveProduct";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Long way
+                var parameter = new SqlParameter("@id", id);
+                cmd.Parameters.Add(parameter);
+
+                cmd.ExecuteNonQuery();
+            };
         }
 
-        protected override Product UpdateCore( Product existing, Product newItem )
+        protected override Product UpdateCore( Product existing, Product product )
         {
-            throw new NotImplementedException();
-        }
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("UpdateProduct", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.AddWithValue("@id", existing.Id);
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.AddWithValue("@description", product.Description); //Prefered approach
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@isDiscontinued", product.IsDiscontinued);
+
+                cmd.ExecuteNonQuery();
+            }
+            return GetCore(existing.Id);
+         }
 
         private SqlConnection OpenDatabase ()
         {
